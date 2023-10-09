4c9f9115c691a16154feacdbd53e473962c878c8
dhananja <dhananjay.com>
2023-10-09 12:56:05 +0000

    hhhh

---
+++ package.js
@@ -0,0 +1,18 @@
+[10:56] Fortune Cloud Technologies
+    
+
+{​​​​​​​​
+"name": "node-app",
+"description": "hello jenkins test app",
+"version": "0.0.1",
+"private": true,
+"dependencies": {​​​​​​​​
+"express": "3.12.0"
+   }​​​​​​​​,
+"devDependencies": {​​​​​​​​
+"mocha": "1.20.1",
+"supertest": "0.13.0"
+   }​​​​​​​​
+  }​​​​​​​​
+
+<https://teams.microsoft.com/l/message/19:CDV52e6_b1xeBqUUbUtIu1x1hUWM_6NncwWaSb6q-Ms1@thread.tacv2/1696829184001?tenantId=88738e75-9e08-47e1-908f-bc12b11c19ad&amp;groupId=617375f8-d91d-48c3-a77c-180724ac19ae&amp;parentMessageId=1696829184001&amp;teamName=Devops 24 July&amp;channelName=General&amp;createdTime=1696829184001&amp;allowXTenantAccess=false>
